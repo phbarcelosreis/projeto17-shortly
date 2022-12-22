f4c818567aacc09b093f582a80b771178e0bb267
Pedro Reis <phbarcelosreis66@gmail.com>
2022-12-22 20:15:40 -0300

    feat: add new validation for signup

--- src/middlewares/checkLog.middleware.js
+++ src/middlewares/checkLog.middleware.js
@@ -1,16 +1,30 @@
 import { signInSchema, signUpSchema } from "../models/model";
 
-export default function checkSignUp(req, res, next) {
+export default async function checkSignUp(req, res, next) {
 
-    const user = req.body
+    const user = req.body;
+    const { email, password, confirmPassword } = req.body;
 
     const validation = signUpSchema.validate(user, { abortEarly: false });
 
     if (validation.error) {
-      const errors = validation.error.details.map((detail) => detail.message);
-      return res.status(422).send(errors);
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
     }
-  
+
+    if (password !== confirmPassword) {
+        return res.sendStatus(400);
+    }
+
+    const checkUser = await connection.query(
+        'SELECT email FROM users WHERE email = $1;',
+        [email]
+    );
+
+    if (checkUser.rowCount) {
+        return res.sendStatus(409);
+    }
+
     next();
 
 }
@@ -22,10 +36,10 @@ export default function checkSignIn(req, res, next) {
     const validation = signInSchema.validate(user, { abortEarly: false });
 
     if (validation.error) {
-      const errors = validation.error.details.map((detail) => detail.message);
-      return res.status(422).send(errors);
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
     }
-  
+
     next();
 
 }
