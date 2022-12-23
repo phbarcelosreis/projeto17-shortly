de248a43696de627833dc54da7345f4a6d3b4760
Pedro Reis <phbarcelosreis66@gmail.com>
2022-12-23 13:08:16 -0300

    feat: add dotenv for db

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,8 @@
-import pkg from 'pg';
+import pg from 'pg';
+import dotenv from "dotenv";
+dotenv.config();
 
-const { Pool } = pkg;
+const { Pool } = pg;
 
 const connection = new Pool({
   connectionString: process.env.DATABASE_URL
